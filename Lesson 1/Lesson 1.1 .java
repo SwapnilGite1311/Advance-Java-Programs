f7190204b9e31e2ad77c44e1f6c64eecc230aea1
SwapnilGite1311 <162311616+SwapnilGite1311@users.noreply.github.com>
2024-09-22 21:32:00 +0530

    Update Lesson 1.1 .java

--- Lesson 1/Lesson 1.1 .java
+++ Lesson 1/Lesson 1.1 .java
@@ -1,1 +1,21 @@
+import java.io.*;
+import java.awt.*;
+import java.applet.*;
 
+/*<applet code="app1" width=200 height=200> </applet>*/
+
+//creating applet and setting background color to it
+
+public class app1 extends Applet
+{
+  public void init()
+  {
+    setBackground(Color.gray);
+    setForeground(Color.white);
+  }
+  public void paint(Graphics g)
+  {  
+    g.drawString("Welcome to my first applet",20,50);
+    g.drawString("Welcome to Applet Windows",20,80);
+  }
+}
